73ba8f909b0e46f49eff28e2892023da030c27a4
pedrocgr <pedroc.gr@hotmail.com>
2023-08-20 19:28:31 -0300

    fix router.py

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,9 +1,5 @@
 from fastapi import APIRouter
-<<<<<<< HEAD
-from src.api import items, reviews, songs
-=======
 from src.api import reviews, songs, albums
->>>>>>> main
 
 api_router = APIRouter()
 api_router.include_router(reviews.router, prefix="/reviews", tags=["reviews"])
@@ -11,4 +7,3 @@ api_router.include_router(songs.router, prefix="/songs", tags=["songs"])
 <<<<<<< HEAD
 =======
 api_router.include_router(albums.router, prefix="/albums", tags=["albums"])
->>>>>>> main
